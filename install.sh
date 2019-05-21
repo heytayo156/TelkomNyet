8c0f2b075b12d102855c338c2cd3ff1a757ce363
JafarArif <50292888+heytayo156@users.noreply.github.com>
2019-05-21 13:33:46 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,42 @@
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+pth='\033[00m' #putih
+echo ""
+clear
+python2 katakunci.py
+echo ""
+echo ""
+clear
+echo $me"Type Install [1]"
+echo ""
+pkg install php
+echo ""
+echo $me"Type Install [2]"
+echo ""
+pkg install openssh
+echo ""
+echo $me"Type Install [3]"
+echo ""
+pkg install termcolor
+clear
+echo $bi"                      >>Thanks To<<              "
+echo $pth"     ______________"
+echo $i"     ##############"
+echo $pth"     ••••••••••••••                        _|#|_"
+echo $pur"        /#/                                 |#|"
+echo $pth"       /#/          |~|||    |||~|    •|•|•|•|•|•|•|"
+echo $cy"      /#/           |~| ||  || |~|    •|    |#|"
+echo $pth"     /#/            |~|  ||||  |~|    •|    |#|"
+echo $me"     \#\      /#/   |~|   ||   |~|    •|    |#|"
+echo $pth"      \#\    /#/    |~|        |~|    •|•|•|•|•|•|•|"
+echo $pu"       \#\  /#/     |~|        |~|          |#|   •|"
+echo $pth"        \#\/#/      |~|        |~|          |#|   •|"
+echo $ku"          /#/       |~|        |~|          |#|   •|"
+echo $pth"         /#/                          •|•|•|•|•|•|•|"
+echo $bi"        /#/                                 |#|"
+ec
